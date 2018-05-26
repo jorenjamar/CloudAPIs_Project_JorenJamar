1aee8cd9597dc5d5b9ad6e711bfe76274d05dceb
Joren <jorenjamar@hotmail.com>
2018-05-26 19:28:06 +0200

    zoeken op id toegevoegd aan console resource

--- api/api/Controllers/ConsoleController.cs
+++ api/api/Controllers/ConsoleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace api.Controllers
@@ -30,6 +31,28 @@ namespace api.Controllers
             context.Consoles.Add(newConsole);
             context.SaveChanges();
             return Created("", newConsole);
+        }
+
+        [Route("{id}")] //api/v1/consoles/1
+        [HttpGet]
+        public IActionResult GetGame(int id)
+        {
+            var console = context.Consoles.Find(id);
+            if (console == null)
+                return NotFound();
+
+            return Ok(console);
+        }
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteConsole(int id)
+        {
+            var console = context.Consoles.Find(id);
+
+            //console verwijderen ..
+            context.Consoles.Remove(console);
+            context.SaveChanges();
+            return NoContent();
         }
     }
 }
